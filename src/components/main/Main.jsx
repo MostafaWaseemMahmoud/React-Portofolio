7acd1f22f7c8f108d1b63bf99b9b8b1be30cdec1
MostafaWaseemMahmoud <mostafawaseem22@gmail.com>
2024-09-26 00:06:47 +0300

    Init Application To Deploy

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import './main.css'
+import './main.css';
+
 const Main = () => {
   return (
     <div className='main'>
@@ -8,7 +9,9 @@ const Main = () => {
       </div>
 
       <div className="main-description">
-        <p>Hello, my name is Mostafa Waseem Mahmoud, and I am a 13-year-old MERN Stack web developer. Despite my young age, I have a solid understanding of full-stack web development using the MERN Stack, which includes MongoDB, Express.js, React.js, and Node.js. I have hands-on experience in building dynamic and responsive web applications, from back-end server setups to front-end user interfaces. I am passionate about coding and continuously learning new technologies to enhance my skills. My goal is to create efficient, scalable, and user-friendly applications that solve real-world problems And I Got Cs50 Course With <a href="ttps://certificates.cs50.io/f3fb964f-d41c-420c-add6-123a62074e14.pdf?size=letter" target='_blank'>certifcate</a>.</p>
+        <p>
+          Hello, my name is Mostafa Waseem Mahmoud, and I am a 13-year-old MERN Stack web developer. Despite my young age, I have a solid understanding of full-stack web development using the MERN Stack, which includes MongoDB, Express.js, React.js, and Node.js. I have hands-on experience in building dynamic and responsive web applications, from back-end server setups to front-end user interfaces. I am passionate about coding and continuously learning new technologies to enhance my skills. My goal is to create efficient, scalable, and user-friendly applications that solve real-world problems. I completed the CS50 course and earned a <a href="https://certificates.cs50.io/f3fb964f-d41c-420c-add6-123a62074e14.pdf?size=letter" target='_blank'>certificate</a>.
+        </p>
       </div>
 
       <div className="my-skills">
@@ -19,64 +22,61 @@ const Main = () => {
           <div className="column">
             <div className="card">
               <div className="card-heading">
-                <div className="icon"><img src="https://mostfasyntax.com/storage/categories/GO5Z8lCzgy2stm59pi16ETKVcZDwoNBxdxk0Wri1.svg" alt="" /></div>
+                <div className="icon"><img src="/images/tools.svg" alt="Tools" /></div>
                 <div className="title"><h1>Tools</h1></div>
               </div>
               <div className="card-skills">
-                  <ul>
-                    <li>Post Man</li>
-                    <li>Vscode</li>
-                    <li>Command Line</li>
-                    <li>Sublime Text</li>
-                  </ul>
+                <ul>
+                  <li>Postman</li>
+                  <li>VS Code</li>
+                  <li>Command Line</li>
+                  <li>Sublime Text</li>
+                </ul>
               </div>
             </div>
 
-
             <div className="card">
               <div className="card-heading">
-                <div className="icon"><img src="https://mostfasyntax.com/storage/categories/UmX2rgsQTiN7xFLBMGBfoxJx7X9CKGDElSwxrad2.svg" alt="" /></div>
+                <div className="icon"><img src="/images/languages.svg" alt="Languages" /></div>
                 <div className="title"><h1>Languages</h1></div>
               </div>
               <div className="card-skills">
-                  <ul>
-                    <li>Html/Css</li>
-                    <li>Scss</li>
-                    <li>VanillaJs</li>
-                  </ul>
+                <ul>
+                  <li>HTML/CSS</li>
+                  <li>SCSS</li>
+                  <li>Vanilla JS</li>
+                </ul>
               </div>
             </div>
           </div>
 
-
-
           <div className="column">
             <div className="card">
               <div className="card-heading">
-                <div className="icon"><img src="https://mostfasyntax.com/storage/categories/GO5Z8lCzgy2stm59pi16ETKVcZDwoNBxdxk0Wri1.svg" alt="" /></div>
+                <div className="icon"><img src="/images/libraries.svg" alt="Libraries" /></div>
                 <div className="title"><h1>Libraries</h1></div>
               </div>
               <div className="card-skills">
-                  <ul>
-                    <li>React</li>
-                    <li>NodeJs</li>
-                    <li>Express</li>
-                    <li>BootStrap</li>
-                    <li>Multer</li>
-                  </ul>
+                <ul>
+                  <li>React</li>
+                  <li>Node.js</li>
+                  <li>Express</li>
+                  <li>Bootstrap</li>
+                  <li>Multer</li>
+                </ul>
               </div>
             </div>
-            
+
             <div className="card">
               <div className="card-heading">
-                <div className="icon"><img src="https://mostfasyntax.com/storage/categories/GO5Z8lCzgy2stm59pi16ETKVcZDwoNBxdxk0Wri1.svg" alt="" /></div>
-                <div className="title"><h1>DataBase</h1></div>
+                <div className="icon"><img src="/images/database.svg" alt="Database" /></div>
+                <div className="title"><h1>Database</h1></div>
               </div>
               <div className="card-skills">
-                  <ul>
-                    <li>MongoDb</li>
-                    <li>Mongoose</li>
-                  </ul>
+                <ul>
+                  <li>MongoDB</li>
+                  <li>Mongoose</li>
+                </ul>
               </div>
             </div>
           </div>
@@ -88,167 +88,80 @@ const Main = () => {
           <h1 className="heading">Projects</h1>
         </div>
         <div className="projects-cards">
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/Chat Application.png" alt="" />
+          <div className="card project-card">
+            <div className="card-img">
+              <img src="/images/chat-application.png" alt="Chat Application" />
+            </div>
+            <div className="card-txt">
+              <div className="title">
+                <h1>Chat Application</h1>
               </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>Chat Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(HTML,CSS,VanillaJs)</p>
-                  <p>(NodeJs,Express,MongoDB)</p>
-                  <div className="links">
-
+              <div className="card-description">
+                <p>(HTML, CSS, VanillaJS)</p>
+                <p>(Node.js, Express, MongoDB)</p>
+                <div className="links">
                   <h3>
-                  <a target='_blank' href="https://youtu.be/gAKsZvU4G-g?si=yPN4eXEhEYLnBD63">
-                  <i class="fa-brands fa-youtube"></i>
-                  </a>
+                    <a target='_blank' href="https://youtu.be/gAKsZvU4G-g?si=yPN4eXEhEYLnBD63">
+                      <i className="fa-brands fa-youtube"></i>
+                    </a>
                   </h3>
-                  <h3>
-                  </h3>
-                  </div>
                 </div>
               </div>
+            </div>
           </div>
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/covid-19.png" alt="" />
-              </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>Kovid-19 Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(React, Css, ReactIcons)</p>
-                  <div className="links">
 
+          <div className="card project-card">
+            <div className="card-img">
+              <img src="/images/covid-19.png" alt="Covid-19 Application" />
+            </div>
+            <div className="card-txt">
+              <div className="title">
+                <h1>Covid-19 Application</h1>
+              </div>
+              <div className="card-description">
+                <p>(React, CSS, React Icons)</p>
+                <div className="links">
                   <h3>
-                  <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/Kovid-App">
-                  <i class="fa-brands fa-github"></i>
-                  </a>
+                    <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/Kovid-App">
+                      <i className="fa-brands fa-github"></i>
+                    </a>
                   </h3>
                   <h3>
                     <a target='_blank' href="https://kovid-app.netlify.app/">
-                    <i class="fa-solid fa-life-ring"></i></a>
+                      <i className="fa-solid fa-life-ring"></i>
+                    </a>
                   </h3>
-                  </div>
                 </div>
               </div>
-        </div>
+            </div>
+          </div>
 
-
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/EmailSender.png" alt="" />
+          <div className="card project-card">
+            <div className="card-img">
+              <img src="/images/email-sender.png" alt="Email Sender Application" />
+            </div>
+            <div className="card-txt">
+              <div className="title">
+                <h1>Email Sender Application</h1>
               </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>Send Email Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(React, EmailJs)</p>
-                  <div className="links">
+              <div className="card-description">
+                <p>(React, EmailJS)</p>
+                <div className="links">
                   <h3>
-                  <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/Send-Email-With-React">
-                  <i class="fa-brands fa-github"></i>
-                  </a>
+                    <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/Send-Email-With-React">
+                      <i className="fa-brands fa-github"></i>
+                    </a>
                   </h3>
                   <h3>
                     <a target='_blank' href="https://send-email-with-react.netlify.app/">
-                    <i class="fa-solid fa-life-ring"></i></a>
+                      <i className="fa-solid fa-life-ring"></i>
+                    </a>
                   </h3>
-                  </div>
                 </div>
               </div>
-        </div>
+            </div>
+          </div>
 
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/Mobilexx.png" alt="" />
-              </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>Mobile Shop Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(HtMl,Css,VanillaJs)</p>
-                  <div className="links">
-                  <h3>
-                  <a target='_blank' href="https://mobilexx-v2.netlify.app/">
-                    <i class="fa-solid fa-life-ring"></i>
-                  </a>
-                  </h3>
-                  </div>
-                </div>
-              </div>
-        </div>
-
-
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/Authrization Application.png" alt="" />
-              </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>Authrization Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(ReactJs, Auth0, Vite)</p>
-                  <div className="links">
-                  <h3>
-                  <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/Authrization-With-React">
-                  <i class="fa-brands fa-github"></i>
-                  </a>
-                  </h3>
-                  </div>
-                </div>
-              </div>
-        </div>
-
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/TomatoApi.png" alt="" />
-              </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>Tomato BackEnd Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(NodeJs,Express,MongoDb)</p>
-                  <div className="links">
-                  <h3>
-                  <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/Tomato-Api">
-                  <i class="fa-brands fa-github"></i>
-                  </a>
-                  </h3>
-                  </div>
-                </div>
-              </div>
-        </div>
-
-        <div className="card project-card">
-              <div className="card-img">
-                <img src="/FaceBook.png" alt="" />
-              </div>
-              <div className="card-txt">
-                <div className="title">
-                  <h1>FaceBook Full Small Application</h1>
-                </div>
-                <div className="card-description">
-                  <p>(ReactJs)</p>
-                  <p>(NodeJs,Express,MongoDb)</p>
-                  <div className="links">
-                  <h3>
-                  <a target='_blank' href="https://github.com/MostafaWaseemMahmoud/FaceBook-React">
-                  <i class="fa-brands fa-github"></i>
-                  </a>
-                  </h3>
-                  </div>
-                </div>
-              </div>
-        </div>
         </div>
       </div>
     </div>
